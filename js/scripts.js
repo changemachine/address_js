a88b6d695e3b79144885a78a369ebf5cd8978fe3
Reid Baldwin & Jill Kuchman <student@epicodus.com>
2015-04-13 10:35:35 -0700

    Add click function to address book list items

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,19 @@ $(document).ready(function() {
                         " " + newContact.lastName +
                         "</span></li>");
 
-        $("input#new-first-name").val("");
-        $("input#new-last-name").val("");
-        $("input#new-address").val("");
+        $("input#new-first-name").val();
+        $("input#new-last-name").val();
+        $("input#new-address").val();
+
+        $(".contact").last().click(function() {
+            $("#show-contact").show();
+            $("show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+            $(".address").text(newContact.address);
+        });
+
     });
+
+
 });
