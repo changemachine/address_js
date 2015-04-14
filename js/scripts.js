ca455ef36ff301ca217e1562603fddc600043e24
Jill Kuchman & James Nielson <student@epicodus.com>
2015-04-14 09:51:03 -0700

    Add input for street, city, state, zip, and method for full address

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,25 @@ $(document).ready(function() {
 
         var inputtedFirstName = $("input#new-first-name").val();
         var inputtedLastName =  $("input#new-last-name").val();
-        var inputtedAddress = $("input#new-address").val();
-        var newContact = {firstName: inputtedFirstName,
-                          lastName: inputtedLastName,
-                          address: inputtedAddress };
+        var inputtedStreet = $("input#new-street").val();
+        var inputtedCity = $("input#new-city").val();
+        var inputtedState = $("input#new-state").val();
+        var inputtedZip = $("input#new-zip").val();
+
+        var newAddress =    {   street: inputtedStreet,
+                                city: inputtedCity,
+                                state: inputtedState,
+                                zip: inputtedZip,
+                                fullAddress: function() {
+                                    return this.street + ", " + this.city + ", " + this.state + ", " + this.zip;
+                                }
+                            };
+
+        var newContact = {
+                            firstName: inputtedFirstName,
+                            lastName: inputtedLastName,
+                            address: newAddress.fullAddress()
+                         };
 
         $("ul#contacts").append("<li><span class='contact'>" +
                         newContact.firstName +
@@ -16,7 +31,13 @@ $(document).ready(function() {
 
         $("input#new-first-name").val();
         $("input#new-last-name").val();
-        $("input#new-address").val();
+        $("input#new-street").val();
+        $("input#new-city").val();
+        $("input#new-state").val();
+        $("input#new-zip").val();
+
+
+
 
         $(".contact").last().click(function() {
             $("#show-contact").show();
